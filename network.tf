23584387f1a98143ceaf68f7e4e36bb32cbebc70
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-10 14:31:36 +0530

    adding final code

--- network.tf
+++ network.tf
@@ -15,19 +15,6 @@ data "ibm_is_vpc" "f5_vpc" {
   name = "${var.vpc_name}"
 }
 
-##############################################################################
-# Create Public_Gateway for a given VPC
-##############################################################################
-resource "ibm_is_public_gateway" "f5_gateway" {
-  name = "f5-bigip-1nic-demo-gateway01"
-  vpc  = "${data.ibm_is_vpc.f5_vpc.id}"
-  zone = "${data.ibm_is_zone.zone.name}"
-
-  //User can configure timeouts
-  timeouts {
-    create = "5m"
-  }
-}
 /*
 ##############################################################################
 # Create Subnet for a given VPC and Public-Gateway
@@ -42,12 +29,5 @@ resource "ibm_is_subnet" "f5_subnet1" {
 */
 
 data "ibm_is_subnet" "f5_subnet1"{
-   identifier = "${var.subnet_name}"
-}
-##############################################################################
-# Create Floating-IP targeted to F5-BIGIP virtual server primary network
-##############################################################################
-resource ibm_is_floating_ip "f5_fip01" {
-  name   = "f5-bigip-1nic-demo-ip01"
-  target = "${ibm_is_instance.f5_vsi.primary_network_interface.0.id}"
+   identifier = "${var.subnet_id}"
 }
