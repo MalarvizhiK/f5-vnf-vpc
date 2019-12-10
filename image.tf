9f8a8761b4b524b718d02c51621d1c27ba1103f5
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-10 17:15:36 +0530

    custom image changes

--- image.tf
+++ image.tf
@@ -46,8 +46,13 @@ variable "images_values_condn" {
   value = "${contains(var.images_values, var.f5_image_name)}"
 }
 
+locals {
+  images_values_id = "${contains(var.images_values, var.f5_image_name)}"
+}
+
+
 resource "ibm_is_image" "f5_custom_image" {
-  count = contains(var.images_values, var.f5_image_name) ? 0 : 1
+  count = "${locals.images_values_id}" == "true" ? 0 : 1
   depends_on       = ["ibm_iam_authorization_policy.authorize_image"]
   href             = "${var.vnf_f5bigip_cos_image_url}"
   name             = "${var.f5_image_name}"
