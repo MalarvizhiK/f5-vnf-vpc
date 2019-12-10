451587da0ab23ee243f9d53ccdac3550db46dccd
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-10 16:32:17 +0530

    custom image changes

--- image.tf
+++ image.tf
@@ -35,15 +35,15 @@ resource "ibm_iam_authorization_policy" "authorize_image" {
   target_resource_instance_id = "${var.vnf_f5bigip_cos_instance_id}"
 }
 
-data "ibm_is_image" "f5_custom_image_data" {
-  name = "${var.f5_image_name}"
+data "ibm_is_images" "f5_custom_images_data" {
 }
 
-output "print_image" {
-  value = "${data.ibm_is_image.f5_custom_image_data.id}"
+variable "images_values" {
+  value = "${data.ibm_is_image.f5_custom_images_data["name"].values}"
 }
 
 resource "ibm_is_image" "f5_custom_image" {
+  count = "${contains("${var.images_values}", "${var.f5_image_name}")}" ? 0 : 1
   depends_on       = ["ibm_iam_authorization_policy.authorize_image"]
   href             = "${var.vnf_f5bigip_cos_image_url}"
   name             = "${var.f5_image_name}"
