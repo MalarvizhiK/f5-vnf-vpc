306bc4d47f61d83a5c4613897528301966f28371
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-11 22:16:07 +0530

    change in condition

--- image.tf
+++ image.tf
@@ -72,7 +72,8 @@ data "external" "find_custom_image" {
 
 
 resource "ibm_is_image" "f5_custom_image" {
-  count = "${lookup(data.external.find_custom_image.result, "id")}"
+  // count = "${lookup(data.external.find_custom_image.result, "id")}"
+  count = 0 
   depends_on       = ["ibm_iam_authorization_policy.authorize_image", "data.external.find_custom_image"]
   href             = "${var.vnf_f5bigip_cos_image_url}"
   name             = "${var.f5_image_name}"
