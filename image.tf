dc73c3fb91e915a78681e93bc27062941ec8b334
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2019-12-11 11:49:31 +0530

    added script

--- image.tf
+++ image.tf
@@ -38,7 +38,11 @@ resource "ibm_iam_authorization_policy" "authorize_image" {
 data "ibm_is_images" "f5_custom_images_data" {
 }
 
+/*
 variable "images_values" {
+  type = list
+  count = "${length(data.ibm_is_images.f5_custom_images_data)}"
+  value     = "${lookup(element(data.ibm_is_images.f5_custom_images_data,count.index),"name")}"
   value = "${data.ibm_is_image.f5_custom_images_data[*]["name"].values}"
 }
 
@@ -49,10 +53,20 @@ variable "images_values_condn" {
 locals {
   images_values_id = "${contains(var.images_values, var.f5_image_name)}"
 }
+*/
+
+data "external" "find_custom_image" {
+  depends_on = ["data.ibm_is_vpc.f5_vpc"]
+  program    = ["bash", "${path.module}/scripts/find_custom_image.sh"]
+
+  query = {
+  }
+}
+
 
 
 resource "ibm_is_image" "f5_custom_image" {
-  count = "${locals.images_values_id}" == "true" ? 0 : 1
+  count = 0
   depends_on       = ["ibm_iam_authorization_policy.authorize_image"]
   href             = "${var.vnf_f5bigip_cos_image_url}"
   name             = "${var.f5_image_name}"
